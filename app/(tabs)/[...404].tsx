872ab9a2f4a5226f069e52c7c619f6991e090118
Nizav <nizav.business@gmail.com>
2025-07-30 18:37:13 +0700

    fix: delete 404 on the (tabs)

--- app/(tabs)/[...404].tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function NotFoundCatchAll() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Screen not found. Go to home screen!</Text>
-    </View>
-  );
-}
